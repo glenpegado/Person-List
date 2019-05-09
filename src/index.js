412e55b773e45246b9462e286ce5c71e5a4fe833
Glen Pegado <glenpegado@gmail.com>
2019-05-09 15:32:07 -0700

    Just Added React profile page components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const Person = ({img, name, job, children}) => {
+    const url = `https://randomuser.me/api/portraits/men/${img}.jpg`
+    return(
+        <article className="person">
+            < img src = {url} alt ="person" />  
+            <h4>{name}</h4>
+            <h4>{job}</h4>
+            {children}
+        </article>
+    )
+}
+
+
+const PersonList = () =>{
+    return(
+        <section className="person-list">
+            <Person img="34" name="john" job="developer"/>
+            <Person img = "23" name = "glen" job = "dancer">
+            < p > Lorem ipsum dolor sit amet consectetur, adipisicing elit.Esse architecto ipsa, officia voluptatem eos voluptatibus temporibus praesentium aut qui iste, quod est enim quas maxime vel totam dolor neque ut! </p>
+            </Person>
+            < Person img = "42" name = "bob" job = "athelete" />
+
+        </section>
+    )
+}
+
+ReactDOM.render(<PersonList/>,document.getElementById('root') )
